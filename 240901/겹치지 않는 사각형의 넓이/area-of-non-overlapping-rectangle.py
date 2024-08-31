27f21f9292e84879436a2d71a970f2cc6d98ef0e
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-01 06:14:24 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240901/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -0,0 +1,30 @@
+size = 22
+offset = size // 2
+
+a_x1, a_y1, a_x2, a_y2 = map(int, input().split())
+
+b_x1, b_y1, b_x2, b_y2 = map(int, input().split())
+
+m_x1, m_y1, m_x2, m_y2 = map(int, input().split())
+
+matrix = [[0] * size for _ in range(size)]
+
+for i in range(a_x1, a_x2):
+    for j in range(a_y1, a_y2):
+        matrix[i + offset][j + offset] = 1
+
+for i in range(b_x1, b_x2):
+    for j in range(b_y1, b_y2):
+        matrix[i + offset][j + offset] = 1
+
+for i in range(m_x1, m_x2):
+    for j in range(m_y1, m_y2):
+        matrix[i + offset][j + offset] = 0
+
+result = 0
+
+for i in range(size):
+    for j in range(size):
+        result += matrix[i][j]
+
+print(result)
