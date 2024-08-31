4f4c48e3496ff1d9718014ae8d44ca8ff4503395
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-01 06:23:30 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240901/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,28 @@
+size = 2001
+offset = size // 2
+
+a_x1, a_y1, a_x2, a_y2 = map(int, input().split())
+b_x1, b_y1, b_x2, b_y2 = map(int, input().split())
+
+matrix = [[0] * size for _ in range(size)]
+
+for i in range(a_x1, a_x2 + 1):
+    for j in range(a_y1, a_y2 + 1):
+        matrix[i + offset][j + offset] = 1
+
+for i in range(b_x1, b_x2 + 1):
+    for j in range(b_y1, b_y2 + 1):
+        matrix[i + offset][j + offset] = 0
+
+min_x, min_y, max_x, max_y = size + 1, size + 1, -1, -1
+
+x_points = []
+y_points = []
+
+for i in range(size):
+    for j in range(size):
+        if matrix[i][j] == 1:
+            x_points.append(i)
+            y_points.append(j)
+
+print((max(x_points) - min(x_points)) * (max(y_points) - min(y_points)))
