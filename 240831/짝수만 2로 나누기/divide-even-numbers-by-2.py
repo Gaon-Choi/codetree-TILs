5339878564fda687de5c48a22ddaadef879ee1a5
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-08-31 17:12:06 +0900

    Title: 짝수만 2로 나누기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240831/짝수만 2로 나누기/divide-even-numbers-by-2.py
+++ 240831/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -4,7 +4,7 @@ num_list = list(map(int, input().split()))
 
 for i in range(len(num_list)):
     if num_list[i] % 2 == 0:
-        num_list[i] /= 2
+        num_list[i] = num_list[i] // 2
 
 for elem in num_list:
     print(elem, " ", sep="", end="")
