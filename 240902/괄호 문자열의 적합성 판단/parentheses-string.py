86fc722b1540b9fc89abb75050b6342f14223fbf
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-02 11:09:32 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240902/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 240902/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -6,13 +6,10 @@ for q in query:
     if q == "(":
         brace_stack.append('(')
     else:
-        if len(brace_stack) == 0:
-            print("No")
-            exit(0)
         if brace_stack[-1] == ")":
             brace_stack.pop()
-        else:
-            print("No")
-            exit(0)
 
-print("Yes")
+if not brace_stack:
+    print("Yes")
+else:
+    print("No")
