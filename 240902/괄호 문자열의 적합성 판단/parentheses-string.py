c475549808dcb2f9fecbf0eaa7e50825848ec026
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-02 11:10:13 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240902/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 240902/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -6,7 +6,7 @@ for q in query:
     if q == "(":
         brace_stack.append('(')
     else:
-        if brace_stack[-1] == ")":
+        if brace_stack[-1] == "(":
             brace_stack.pop()
 
 if not brace_stack:
