d9f32f1bcd22a2b2b12946e63bc4eca1cba72ec8
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-02 12:33:24 +0900

    Title: 수열 조작, Time: 203ms, Memory: 38MB, Status: Passed - CodeTree

--- 240902/수열 조작/sequence-manipulation.py
+++ 240902/수열 조작/sequence-manipulation.py
@@ -1,16 +1,18 @@
-queue = list()
+from collections import deque
+
+dq = deque()
 
 N = int(input())
 
 for i in range(1, N + 1):
-    queue.append(i)
+    dq.append(i)
 
-while (len(queue) > 1):
+while (len(dq) > 1):
     # 맨 앞의 정수 제거
-    queue.pop(0)
+    dq.popleft()
 
     # 남은 수열의 맨 앞의 정수를 맨 뒤로 이동
-    front = queue.pop(0)
-    queue.append(front)
+    front = dq.popleft()
+    dq.append(front)
 
-print(queue[0])
+print(dq[0])
