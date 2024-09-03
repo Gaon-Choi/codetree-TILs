19469717f870427c121602b6f6a9489d32371246
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-03 11:55:37 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240903/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,26 @@
+# S -> E -> N -> W
+dx = [0, +1, 0, -1]
+dy = [+1, 0, -1, 0]
+
+offset = 0
+
+m, n = map(int, input().split())
+
+matrix = [[0] * m for _ in range(n)]
+
+i = 0
+size = m * n
+
+loc = [0, 0]
+
+for i in range(size):
+    matrix[loc[1]][loc[0]] = i + 1
+    if not (0 <= loc[0] + dx[offset % 4] and loc[0] + dx[offset % 4] < n and 0 <= loc[1] + dy[offset % 4] and loc[1] + dy[offset % 4] < n and matrix[loc[1] + dy[offset % 4]][loc[0] + dx[offset % 4]] == 0):
+        offset += 1
+    loc[0] += dx[offset % 4]
+    loc[1] += dy[offset % 4]
+
+for row in matrix:
+    for elem in row:
+        print(elem, end = " ")
+    print()
