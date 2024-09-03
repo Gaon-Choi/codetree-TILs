7849564ddfc9456b5bc65587fbc8cc0ed29c3b44
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-03 10:09:49 +0900

    Title: 더 긴 문자열, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/더 긴 문자열/longer-string.py
+++ 240903/더 긴 문자열/longer-string.py
@@ -1,9 +1,8 @@
-t1 = input()
-t2 = input()
+t1, t2 = input().split()
 
 if len(t1) > len(t2):
-    print(t1)
+    print(t1, len(t1))
 elif len(t1) < len(t2):
-    print(t2)
+    print(t2, len(t2))
 else:
     print("same")
