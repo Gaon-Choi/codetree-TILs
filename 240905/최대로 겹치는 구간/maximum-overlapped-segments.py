7c402ef0a0d43c6a7aaffa6b0d9242554c397984
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-05 21:27:26 +0900

    Title: 최대로 겹치는 구간, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240905/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+array = [0] * (200 + 1)
+
+for _ in range(N):
+    start, end = map(int, input().split())
+    for i in range(start - 1 , end):
+        array[i + 100] += 1
+
+max_dupl = array.count(max(array))
+
+print(array.count(max_dupl))
