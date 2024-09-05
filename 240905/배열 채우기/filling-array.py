5de7615dd6c856c3e79b29ee817cc0cabd35f637
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-05 22:18:35 +0900

    Title: 배열 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240905/배열 채우기/filling-array.py
+++ 240905/배열 채우기/filling-array.py
@@ -1,12 +1,15 @@
 arr = list(map(int, input().split()))
 
 temp = []
+original_length = arr
+cnt = 1
 
 while (True):
-    if arr[0] == 0:
+    if arr[0] == 0 or original_length == cnt:
         break
     else:
         temp.append(arr.pop(0))
+        cnt += 1
 
 temp.reverse()
 
