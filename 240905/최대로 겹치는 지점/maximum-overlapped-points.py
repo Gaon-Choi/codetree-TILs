d906f5d823bedf59f3a94fae2aa20d1e03ddb043
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-05 21:35:37 +0900

    Title: 최대로 겹치는 지점, Time: 148ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240905/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 240905/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -1,6 +1,8 @@
 start = 1
 end = 100
 
+offset = -1
+
 arr = [0] * (end - start)
 
 N = int(input())
@@ -8,6 +10,6 @@ N = int(input())
 for _ in range(N):
     s, e = map(int, input().split())
     for i in range(s, e):
-        arr[i] += 1
+        arr[i + offset] += 1
 
 print(max(arr) + 1)
