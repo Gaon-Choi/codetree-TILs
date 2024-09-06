89254d200a82837b9334b4fa9a567a0d296599e2
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-06 10:00:24 +0900

    Title: 점수대 파악하기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240906/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,10 @@
+result = [0] * 11
+
+arr = list(map(int, input().split()))
+
+for elem in arr:
+    if elem // 10 > 0:
+        result[elem // 10] += 1
+
+for i in range(len(result) - 1, 0, -1):
+    print("{a} - {b}".format(a=i * 10, b = result[i]))
