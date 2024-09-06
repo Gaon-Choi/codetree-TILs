e08617132a23537df47f773474b3d0c98ed988a0
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-06 16:11:12 +0900

    Title: 벌금은 누구에게, Time: 178ms, Memory: 25MB, Status: Passed - CodeTree

--- 240906/벌금은 누구에게/who-will-pay.py
+++ 240906/벌금은 누구에게/who-will-pay.py
@@ -3,14 +3,18 @@ N, M, K = map(int, input().split())
 student = [0] * N
 punish = []
 
+flag = True
+
 for _ in range(M):
     punish.append(int(input()))
 
 for p in punish:
     student[p-1] += 1
-    
+
     if K in student:
         print(student.index(K) + 1)
+        flag = False
         break
 
-print(-1)
+if flag:
+    print(-1)
