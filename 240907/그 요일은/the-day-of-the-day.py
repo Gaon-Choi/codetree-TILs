9d5757702ab163c8e5245ad219b8a18365c2f02c
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-07 14:35:14 +0900

    Title: 그 요일은, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240907/그 요일은/the-day-of-the-day.py
+++ 240907/그 요일은/the-day-of-the-day.py
@@ -7,13 +7,13 @@ weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 m1, d1, m2, d2 = map(int, input().split())
 day = weekday.index(input())
 
-day_1 = sum(month[0 : m1]) + d1
-day_2 = sum(month[0 : m2]) + d2
+day_1 = sum(month[0:m1]) + d1
+day_2 = sum(month[0:m2]) + d2
 
 result = 0
 
-for i in range(day_1, day_2):
-    if i % 7 == day:
+for i in range(day_1, day_2 + 1):
+    if (i % 7) == day:
         result += 1
 
 print(result)
