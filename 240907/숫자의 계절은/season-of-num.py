08a227fb59cb980ab6cb56ef459fbf888cfa7b7d
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-07 19:54:58 +0900

    Title: 숫자의 계절은, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240907/숫자의 계절은/season-of-num.py
+++ 240907/숫자의 계절은/season-of-num.py
@@ -4,7 +4,7 @@ if 3 <= m and m <= 5:
     print("Spring")
 elif 6 <= m and m <= 8:
     print("Summer")
-if 9 <= m and m <= 11:
+elif 9 <= m and m <= 11:
     print("Fall")
 else:
     print("Winter")
