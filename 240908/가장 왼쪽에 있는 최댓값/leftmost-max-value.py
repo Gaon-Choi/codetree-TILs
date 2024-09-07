9c9d59569d6d4bb995745a282ab21f7a996571f7
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-08 03:34:49 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240908/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+locations = []
+
+while(len(arr) > 1):
+    maxima_idx = arr.index(max(arr))
+    locations.append(maxima_idx + 1)
+    arr = arr[0:maxima_idx]
+
+for elem in locations:
+    print(elem, end=" ")
