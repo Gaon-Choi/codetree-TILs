ee0f9a51f9df4d649cb2e903e4750f7a24950dd3
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-07 14:08:33 +0900

    Title: 요일 맞추기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240907/요일 맞추기/guess-day-of-week.py
+++ 240907/요일 맞추기/guess-day-of-week.py
@@ -4,7 +4,7 @@ weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 m1, d1, m2, d2 = map(int, input().split())
 
-day_1 = month[m1 - 1] + d1
-day_2 = month[m2 - 1] + d2
+day_1 = sum(month[0 : m1]) + d1
+day_2 = sum(month[0 : m2]) + d2
 
 print(weekday[(day_2 - day_1) % 7])
