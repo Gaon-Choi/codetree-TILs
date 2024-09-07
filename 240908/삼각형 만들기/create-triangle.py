5cbb5a4fe599863eaa93bd8f88125bd399af7811
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-08 02:09:34 +0900

    Title: 삼각형 만들기, Time: 131ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240908/삼각형 만들기/create-triangle.py
+++ 240908/삼각형 만들기/create-triangle.py
@@ -1,3 +1,13 @@
+def is_perpendicular(p1, p2, p3):
+    s1 = (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+    s2 = (p2[0] - p3[0]) ** 2 + (p2[1] - p3[1]) ** 2
+    s3 = (p3[0] - p1[0]) ** 2 + (p3[1] - p1[1]) ** 2
+
+    if s3 == s1 + s2 or s1 == s2 + s3 or s2 == s1 + s3:
+        return True
+    else:
+        return False
+
 def calculate_area(p1, p2, p3):
     return (max(p1[0], p2[0], p3[0]) - min(p1[0], p2[0], p3[0])) * (max(p1[1], p2[1], p3[1]) - min(p1[1], p2[1], p3[1]))
 
@@ -13,6 +23,7 @@ results = []
 for i in range(n):
     for j in range(i + 1, n):
         for k in range(j + 1, n):
-            results.append(calculate_area(points[i], points[j], points[k]))
+            if is_perpendicular(points[i], points[j], points[k]):
+                results.append(calculate_area(points[i], points[j], points[k]))
 
 print(max(results))
