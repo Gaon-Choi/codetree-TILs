8c5e83bdb643ce8faf9f12ad842d1cabc4e52ea9
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-07 23:21:10 +0900

    Title: 작은 구슬의 이동, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240907/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,28 @@
+direction = {
+    "L": [0, -1],
+    "R": [0, +1],
+    "U": [-1, 0],
+    "D": [+1, 0]
+}
+
+n, t = map(int, input().split())
+
+# r행 c열
+r, c, d = input().split()
+r = int(r); c = int(c)
+
+for _ in range(t + 1):
+    if  (1 <= r + direction[d][0]) and (r + direction[d][0] < n) and  (1 <= c + direction[d][1]) and (c + direction[d][1] < n):
+        r += direction[d][0]
+        c += direction[d][1]
+    else:
+        if d == "L":
+            d = "R"
+        elif d == "R":
+            d = "L"
+        elif d == "U":
+            d = "D"
+        else:
+            d = "U"
+
+print(r, c)
