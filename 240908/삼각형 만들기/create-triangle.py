b146d32da17637dae41ce3bb1cccb2123f1220b1
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-08 02:10:17 +0900

    Title: 삼각형 만들기, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240908/삼각형 만들기/create-triangle.py
+++ 240908/삼각형 만들기/create-triangle.py
@@ -3,7 +3,7 @@ def is_perpendicular(p1, p2, p3):
     s2 = (p2[0] - p3[0]) ** 2 + (p2[1] - p3[1]) ** 2
     s3 = (p3[0] - p1[0]) ** 2 + (p3[1] - p1[1]) ** 2
 
-    if s3 == s1 + s2 or s1 == s2 + s3 or s2 == s1 + s3:
+    if (s3 == s1 + s2) or (s1 == s2 + s3) or (s2 == s1 + s3):
         return True
     else:
         return False
