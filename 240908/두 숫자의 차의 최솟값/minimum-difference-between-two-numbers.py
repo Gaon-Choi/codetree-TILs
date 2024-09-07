6e6909c9f687cc00dcf74f5280b617aac7305f35
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-08 03:39:39 +0900

    Title: 두 숫자의 차의 최솟값, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240908/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+results = []
+
+for i in range(n):
+    for j in range(i + 1, n):
+        results.append(arr[j] - arr[i])
+
+print(min(results))
