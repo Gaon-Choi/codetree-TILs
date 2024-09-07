a6d06172d2f5b5b008312a210257403d8e4d4292
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-07 23:26:23 +0900

    Title: 되돌아오기 2, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240907/되돌아오기 2/come-back-2.py
@@ -0,0 +1,25 @@
+r = 0; c = 0
+
+dx = [0, +1, 0, -1]
+dy = [-1, 0, +1, 0]
+
+offset = 0
+
+queries = list(input())
+
+for t, q in enumerate(queries):
+    if q == "F":
+        r += dx[offset % 4]
+        c += dy[offset % 4]
+
+        if r == 0 and c == 0:
+            print(t + 1)
+            break
+
+    elif q == "R":
+        offset += 1
+    else:
+        offset -= 1
+
+if t == len(queries) - 1:
+    print(-1)
