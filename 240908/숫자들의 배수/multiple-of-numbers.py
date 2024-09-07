3786a5f3138ac7209af784151d59fe87ce52bf0c
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-08 03:24:02 +0900

    Title: 숫자들의 배수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240908/숫자들의 배수/multiple-of-numbers.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+is_five_multiple = 0
+arr = [n]
+
+e = n
+
+while (is_five_multiple < 2):
+    e = e + n
+    if e % 5 == 0:
+        is_five_multiple += 1
+    arr.append(e)
+
+
+for elem in arr:
+    print(elem, end=" ")
