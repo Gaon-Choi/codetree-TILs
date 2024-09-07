87400e3a3ed58e50ff1b2a88aaf248012d5e4cb5
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-08 03:38:33 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 145ms, Memory: 32MB, Status: Passed - CodeTree

--- 240908/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240908/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -6,7 +6,8 @@ revenue = []
 
 for i in range(len(arr)):
     for j in range(i+1, len(arr)):
-        revenue.append(arr[j] - arr[i])
+        if arr[j] - arr[i] >= 0:
+            revenue.append(arr[j] - arr[i])
 
 if len(revenue) > 0:
     print(max(revenue))
