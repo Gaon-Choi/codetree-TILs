90c030e5dae768b93537eada30a5cd6498ca3d75
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-08 14:53:06 +0900

    Title: 배열 놀이, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240908/배열 놀이/play-with-array.py
@@ -0,0 +1,21 @@
+n, q = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+for _ in range(q):
+    query = list(map(int, input().split()))
+
+    if query[0] == 1:
+        print(arr[query[1] - 1])
+
+    elif query[0] == 2:
+        search = query[1]
+
+        if search in arr:
+            print(arr.index(search) + 1)
+        else:
+            print(0)
+
+    elif query[0] == 3:
+        for elem in arr[query[1]-1:query[2]]:
+            print(elem, end=" ")
