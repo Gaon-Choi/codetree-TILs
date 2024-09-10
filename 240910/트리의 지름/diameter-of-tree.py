39a451a0aa0a3becb3083f72630479108a9275f7
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-10 21:51:53 +0900

    Title: 트리의 지름, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240910/트리의 지름/diameter-of-tree.py
+++ 240910/트리의 지름/diameter-of-tree.py
@@ -1,6 +1,8 @@
 import sys
 sys.setrecursionlimit(10**3)
 
+START_POINT = None
+
 def dfs(vertex, curr_distance = 0):
 
     farthest_node = vertex
@@ -25,10 +27,12 @@ visited = [False for _ in range(n + 1)]
 
 for _ in range(n-1):
     start, end, cost = map(int, input().split())
+    if START_POINT is None:
+        START_POINT = start
     tree[start].append((end, cost))
     tree[end].append((start, cost))
 
-mx_dist, f_node = dfs(tree[1][0], 0)
+mx_dist, f_node = dfs(START_POINT, 0)
 
 # 방문 정보 초기화
 visited = [False] * (n + 1)
