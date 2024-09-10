f2de2237ec23521055b1483af7f2c6dd625c4ce1
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-10 11:38:37 +0900

    Title: DateTime to DateTime, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240910/DateTime to DateTime/datetime-to-datetime.py
+++ 240910/DateTime to DateTime/datetime-to-datetime.py
@@ -6,7 +6,7 @@ DAY = HOUR * 24
 current = DAY * a + HOUR * b + c
 eleven = DAY * 11 + HOUR * 11 + 11
 
-if current - eleven > 0:
+if current - eleven >= 0:
     print(current - eleven)
 else:
     print(-1)
