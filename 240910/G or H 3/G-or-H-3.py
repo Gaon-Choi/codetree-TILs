c182709de1387cb794b3a2dd00db60c58c7059ad
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-10 23:52:09 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240910/G or H 3/G-or-H-3.py
@@ -0,0 +1,21 @@
+N, K = map(int, input().split())
+
+max_size = 100
+
+arr = [0] * (max_size + 1)
+
+for _ in range(N):
+    loc, alphabet = input().split()
+    loc = int(loc)
+
+    if alphabet == "G":
+        arr[loc] = 1
+    else:
+        arr[loc] = 2
+
+results = []
+
+for i in range(max_size - K + 1):
+    results.append(sum(arr[i:i+K+1]))
+
+print(max(results))
