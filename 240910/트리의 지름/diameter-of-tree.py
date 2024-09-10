0a7129b5ed5581660a01caef9cc4a1e76da55ad5
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-10 21:41:39 +0900

    Title: 트리의 지름, Time: 237ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240910/트리의 지름/diameter-of-tree.py
@@ -0,0 +1,30 @@
+def dfs(vertex, curr_distance = 0):
+
+    farthest_node = vertex
+    max_distance = curr_distance
+
+    # adjacent list
+    for v, cost in tree[vertex]:
+        if not visited[v]:
+            #parent[curr_v] = vertex
+            visited[v] = True
+            distance, node = dfs(v, curr_distance + cost)
+            if distance > max_distance:
+                max_distance = distance
+                farthest_node = node
+    
+    return max_distance, farthest_node
+
+n = int(input())
+
+tree = [[] * (n + 1) for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
+
+for _ in range(n-1):
+    start, end, cost = map(int, input().split())
+    tree[start].append((end, cost))
+    tree[end].append((start, cost))
+
+mx_dist, f_node = dfs(1, 0)
+
+print(dfs(f_node, mx_dist)[0])
