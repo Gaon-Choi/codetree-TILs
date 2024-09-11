6f9d9ffd6cc97cd19bffa3b605e89775f83d3396
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-11 09:54:00 +0900

    Title: 완전수 판별기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240911/완전수 판별기/perfect-number-discriminator.py
+++ 240911/완전수 판별기/perfect-number-discriminator.py
@@ -2,7 +2,7 @@ def is_perfect_number(n: int):
     total = 0
     for i in range(1, n + 1):
         if n % i == 0:
-            total += if
+            total += i
     
     if total == n:
         return True
