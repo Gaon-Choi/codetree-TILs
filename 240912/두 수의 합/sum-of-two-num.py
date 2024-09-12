338e4bcd1cd78569079dee7589e8922546b2289d
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-12 17:43:34 +0900

    Title: 두 수의 합, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240912/두 수의 합/sum-of-two-num.py
+++ 240912/두 수의 합/sum-of-two-num.py
@@ -1,13 +1,15 @@
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
+hashMap = dict()
+
 for elem in arr:
     if elem in hashMap:
         hashMap[elem] += 1
     else:
         hashMap[elem] = 1
 
-hashMap = dict()
+
 
 
 
