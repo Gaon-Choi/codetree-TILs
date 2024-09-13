98d5ead05b36abf69c564d6322e8b8f99a79f451
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-13 11:08:24 +0900

    Title: 공약수의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240913/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,12 @@
+n1 = 1920; n2 = 2880
+
+a, b = map(int, input())
+
+isExists = False
+
+for i in range(a, b + 1):
+    if n1 % i == 0 and n2 % i == 0:
+        isExists = True
+        break
+
+print("1" if isExists else "0")
