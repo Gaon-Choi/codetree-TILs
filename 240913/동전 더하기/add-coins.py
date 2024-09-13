53522f593ae65dd71689c48c8cc7f91da825d213
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-13 10:35:33 +0900

    Title: 동전 더하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240913/동전 더하기/add-coins.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+
+coins = []
+
+for _ in range(n):
+    coins.append(int(input()))
+
+coins.sort(reverse=True)
+
+money = k
+result = 0
+
+for elem in coins:
+    result += money // k
+    money = money % k
+
+print(result)
