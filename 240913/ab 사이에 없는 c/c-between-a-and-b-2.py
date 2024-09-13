eda13bbe38e0bb6ab4adda3a563ffdc574bda3f2
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-13 11:25:15 +0900

    Title: ab 사이에 없는 c, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240913/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240913/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -3,8 +3,8 @@ a, b, c = map(int, input().split())
 isExists = False
 
 for i in range(a, b + 1):
-    if c % i == 0:
+    if i % c == 0:
         isExists = True
         break
 
-print("YES" if isExists else "NO")
+print("YES" if not isExists else "NO")
