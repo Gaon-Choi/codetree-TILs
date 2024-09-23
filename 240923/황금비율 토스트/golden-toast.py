4eceb72abd56126802675cadd1c2dce97a185c90
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-23 14:27:42 +0900

    Title: 황금비율 토스트, Time: 248ms, Memory: 31MB, Status: Passed - CodeTree

--- 240923/황금비율 토스트/golden-toast.py
+++ 240923/황금비율 토스트/golden-toast.py
@@ -24,9 +24,8 @@ for _ in range(m):
             linked_list.pop(pointer)
 
     else:
-        linked_list.insert(pointer + 1, query[1])
-        if 0 <= pointer and pointer < len(linked_list):
-            pointer += 1
+        linked_list.insert(pointer, query[1])
+        pointer += 1
 
 for elem in linked_list:
     print(elem, end="")
