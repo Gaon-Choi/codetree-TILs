77a8e119ce7dc20cace797e29ac495459841472c
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-23 16:32:45 +0900

    Title: 선분 3개 지우기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240923/선분 3개 지우기/remove-three-segments.py
@@ -0,0 +1,29 @@
+def is_intersection(line1, line2):
+    if line1[1] < line2[0] or line2[1] < line1[0]:
+        return False
+    return True
+
+
+n = int(input())
+
+line_list = []
+
+total_number = 0
+
+for _ in range(n):
+    line_list.append(list(map(int, input().split())))
+
+for i in range(n):
+    for j in range(i, n):
+        for k in range(j, n):
+            except_three = [elem for idx, elem in enumerate(line_list) if idx != i and idx != j and idx != k]
+
+            is_ = True
+
+            for p in range(len(except_three) - 1):
+                if is_intersection(except_three[p], except_three[p + 1]):
+                    is_ = False
+                    break
+            total_number += 1 if is_ else 0
+
+print(total_number)
