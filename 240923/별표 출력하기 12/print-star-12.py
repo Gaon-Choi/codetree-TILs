97c30fdbee00f210bdecab0493dd8c42e2470830
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-23 16:48:27 +0900

    Title: 별표 출력하기 12, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/별표 출력하기 12/print-star-12.py
+++ 240923/별표 출력하기 12/print-star-12.py
@@ -3,8 +3,9 @@ n = int(input())
 arr = [[0] * n for _ in range(n)]
 
 for j in range(n):
-    for i in range(j + 1):
-        arr[i][j] = 1
+    if j % 2 == 0:
+        for i in range(j + 1):
+            arr[i][j] = 1
 
 for i in range(n):
     for j in range(n):
