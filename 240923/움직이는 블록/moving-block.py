0ae56b10882728caf80e04e98351fb6c80cb63ba
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-23 14:35:54 +0900

    Title: 움직이는 블록, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 240923/움직이는 블록/moving-block.py
+++ 240923/움직이는 블록/moving-block.py
@@ -5,12 +5,15 @@ arr = []
 for _ in range(N):
     arr.append(int(input()))
 
+# 목표 개수 설정
 average = sum(arr) // N
 
 result = 0
 
 for elem in arr:
+    # 목표 개수보다 적은 개수의 블록이 있는 칸만 검사
     if elem < average:
+        # 부족한 만큼의 개수를 합산
         result += (average - elem)
 
 print(result)
