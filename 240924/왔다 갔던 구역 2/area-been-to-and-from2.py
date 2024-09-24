e8005cbfd72450d73162437dd0c32f495646267a
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-24 13:09:13 +0900

    Title: 왔다 갔던 구역 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240924/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,22 @@
+size = 1000
+
+arr = [0] * (2 * size + 1)
+
+n = int(input())
+
+curr = 0
+
+for _ in range(n):
+    loc, direction = input().split()
+    loc = int(loc)
+
+    if direction == "R":
+        for _ in range(loc):
+            arr[curr + 1] += 1
+            curr += 1
+    else:
+        for _ in range(loc):
+            arr[curr - 1] += 1
+            curr -= 1
+
+print(len(list(filter(lambda x: x >= 2, arr))))
