374b47ca7d7eaec962c7497debf763782f1a475f
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-24 12:31:34 +0900

    Title: 개발자의 능력 3, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/개발자의 능력 3/ability-of-developer-3.py
+++ 240924/개발자의 능력 3/ability-of-developer-3.py
@@ -6,8 +6,8 @@ N = len(arr)
 min_difference = math.inf
 
 for i in range(N):
-    for j in range(i, N):
-        for k in range(j, N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
             sum_1 = arr[i] + arr[j] + arr[k]
             sum_2 = sum(arr) - sum_1
             min_difference = min(min_difference, abs(sum_1 - sum_2))
