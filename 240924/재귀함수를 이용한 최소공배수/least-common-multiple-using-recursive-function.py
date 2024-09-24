3d10bc19b4e7c40abdf2f563da203583d9d15a67
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-24 11:31:21 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240924/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,19 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+def lcm_recursive(num_list: list):
+    if len(num_list) == 2:
+        return lcm(num_list[0], num_list[1])
+    else:
+        return lcm(num_list[0], lcm_recursive(num_list[1:]))
+
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+print(lcm_recursive(arr))
