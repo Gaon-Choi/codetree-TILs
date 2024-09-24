eae9f8d9e53f5a17cedc74df82d9d6b89e24d206
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-24 13:43:13 +0900

    Title: 선두를 지켜라 3, Time: 169ms, Memory: 34MB, Status: Wrong Answer - CodeTree

---
+++ 240924/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,35 @@
+N, M = map(int, input().split())
+
+a = 0
+a_t = 0
+b = 0
+b_t = 0
+
+arr_a = []
+arr_b = []
+
+for _ in range(N):
+    v, t = map(int, input().split())
+    for _ in range(t):
+        a_t += 1
+        a += v
+        arr_a.append(a)
+
+for _ in range(M):
+    v, t = map(int, input().split())
+    for _ in range(t):
+        b_t += 1
+        b += v
+        arr_b.append(b)
+
+temp = []
+
+for i in range(min(len(arr_a), len(arr_b))):
+    winner = 1 if arr_a[i] > arr_b[i] else -1
+
+    if temp and temp[-1] != winner:
+        temp.append(winner)
+    elif not temp:
+        temp.append(winner)
+
+print(len(temp) + 2)
