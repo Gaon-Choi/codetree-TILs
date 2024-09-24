0984865181874bbafbd265b112e343efecd60d69
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-24 12:30:20 +0900

    Title: 개발자의 능력 3, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240924/개발자의 능력 3/ability-of-developer-3.py
+++ 240924/개발자의 능력 3/ability-of-developer-3.py
@@ -1,12 +1,13 @@
 import math
 
 arr = list(map(int, input().split()))
+N = len(arr)
 
 min_difference = math.inf
 
-for i in range(6):
-    for j in range(i, 6):
-        for k in range(j, 6):
+for i in range(N):
+    for j in range(i, N):
+        for k in range(j, N):
             sum_1 = arr[i] + arr[j] + arr[k]
             sum_2 = sum(arr) - sum_1
             min_difference = min(min_difference, abs(sum_1 - sum_2))
