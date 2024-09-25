7e28dd1fcbee03717fd2d00dbe030a207d964c72
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-25 15:50:49 +0900

    Title: 1차원 바람, Time: 189ms, Memory: 28MB, Status: Passed - CodeTree

--- 240925/1차원 바람/The-1D-wind-blows.py
+++ 240925/1차원 바람/The-1D-wind-blows.py
@@ -17,44 +17,44 @@ arr = []
 for _ in range(N):
     arr.append(list(map(int, input().split())))
 
+for _ in range(Q):
+    location, direction = input().split()
+    location = int(location) - 1
 
-location, direction = input().split()
-location = int(location) - 1
+    up = location - 1
+    down = location + 1
 
-up = location - 1
-down = location + 1
+    curr_direction = 1 if direction == "R" else 0
 
-curr_direction = 1 if direction == "R" else 0
-
-if curr_direction == 1:
-    arr[location] = blow_from_right(arr[location])
-else:
-    arr[location] = blow_from_left(arr[location])
-
-up_ = curr_direction
-down_ = curr_direction
-
-while up >= 0:
-    up_ += 1
-    if is_propagatable(arr[up], arr[up+1]):
-        if up_ % 2 == 1:
-            arr[up] = blow_from_right(arr[up])
-        else:
-            arr[up] = blow_from_left(arr[up])
-        up -= 1
+    if curr_direction == 1:
+        arr[location] = blow_from_right(arr[location])
     else:
-        break
+        arr[location] = blow_from_left(arr[location])
 
-while down < N:
-    down_ += 1
-    if is_propagatable(arr[down], arr[down-1]):
-        if down_ % 2 == 1:
-            arr[down] = blow_from_right(arr[down])
+    up_ = curr_direction
+    down_ = curr_direction
+
+    while up >= 0:
+        up_ += 1
+        if is_propagatable(arr[up], arr[up+1]):
+            if up_ % 2 == 1:
+                arr[up] = blow_from_right(arr[up])
+            else:
+                arr[up] = blow_from_left(arr[up])
+            up -= 1
         else:
-            arr[down] = blow_from_left(arr[down])
-        down += 1
-    else:
-        break
+            break
+
+    while down < N:
+        down_ += 1
+        if is_propagatable(arr[down], arr[down-1]):
+            if down_ % 2 == 1:
+                arr[down] = blow_from_right(arr[down])
+            else:
+                arr[down] = blow_from_left(arr[down])
+            down += 1
+        else:
+            break
     
 for i in range(N):
     for j in range(M):
