9ebcc8813cad93eacc6bf0c2707800ff11174271
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-25 17:43:59 +0900

    Title: 왔다 갔던 구역 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240925/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240925/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -11,10 +11,18 @@ for _ in range(N):
     direction = +1 if direction == "R" else -1
 
     for _ in range(dist):
+        now_ = curr
+        next_ = curr + direction
+
+        if next_ < now_:
+            now_, next_ = next_, now_
+
+        key = "{a} {b}".format(a=now_, b=next_)
         curr += direction
-        if curr in walk:
-            walk[curr] += 1
+
+        if key in walk:
+            walk[key] += 1
         else:
-            walk[curr] = 1
+            walk[key] = 1
 
 print(len(list(filter(lambda x : x >= 2, walk.values()))))
