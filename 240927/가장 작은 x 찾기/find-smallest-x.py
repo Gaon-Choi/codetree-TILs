b50751b793267c03949158425317318eb6c7dc41
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-27 12:17:15 +0900

    Title: 가장 작은 x 찾기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240927/가장 작은 x 찾기/find-smallest-x.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+min_arr = []
+max_arr = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    min_arr.append(x)
+    max_arr.append(y)
+
+answer = -1
+
+for x in range(1, 15):
+    num = x
+    is_flag = False
+    for i in range(n):
+        num *= 2
+        if min_arr[i] <= num and num <= max_arr[i]:
+            continue
+        else:
+            is_flag = True
+            break
+
+    if is_flag and (answer > -1):
+        break
+        
+    elif not is_flag:
+        answer = x
+
+print(answer)
