451434292120d873f53f3f42a6e7460f8bfbd149
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-09-27 11:51:19 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 195ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240927/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
+++ 240927/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -7,19 +7,18 @@ temp = []
 
 for i in range(n):
     for j in range(n):
-        if i != j:
-            diffsum = 0
+        diffsum = 0
 
-            temp_arr = copy.deepcopy(arr)
+        temp_arr = copy.deepcopy(arr)
 
-            temp_arr[i] *= 2
+        temp_arr[i] *= 2
 
-            temp_arr.pop(j)
+        temp_arr.pop(j)
 
-            for i in range(len(temp_arr) - 1):
-                diffsum += abs(temp_arr[i] - temp_arr[i+1])
+        for i in range(len(temp_arr) - 1):
+            diffsum += abs(temp_arr[i] - temp_arr[i+1])
 
-            
-            temp.append(diffsum)
+        
+        temp.append(diffsum)
 
 print(min(temp))
