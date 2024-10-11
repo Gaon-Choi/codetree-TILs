c56fd93c9fc2afe944f676f3a32307972bbf5852
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-10-11 13:52:32 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241011/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+matrix = [[0] * n for _ in range(n)]
+
+# 아랫 줄
+matrix[0][0] = arr[0][0]
+
+for i in range(1, n):
+    matrix[i][0] = min(matrix[i-1][0], arr[i][0])
+    matrix[0][i] = min(matrix[0][i-1], arr[0][i])
+
+for i in range(n):
+    for j in range(n):
+        if matrix[i][j] == 0:
+            matrix[i][j] = max(min(matrix[i-1][j], arr[i][j]), min(matrix[i][j-1], arr[i][j]))
+
+# for i in range(n):
+#     for j in range(n):
+#         print(matrix[i][j], end=" ")
+#     print()
+
+print(matrix[n - 1][n - 1])
