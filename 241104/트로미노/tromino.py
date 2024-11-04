c39107c4402350511c15729d1d052a277cb93730
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-11-04 10:03:09 +0900

    Title: 트로미노, Time: 49ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241104/트로미노/tromino.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+
+matrix = []
+
+for _ in range(n):
+    matrix.append(list(map(int, input().split())))
+
+answer = 0
+
+for i in range(n - 1):
+    for j in range(n - 1):
+        answer = max(answer, matrix[i][j]+matrix[i+1][j]+matrix[i+1][j+1])
+
+for i in range(n - 1):
+    for j in range(n - 1):
+        answer = max(answer, matrix[i][j+1]+matrix[i+1][j+1]+matrix[i+1][j+1])
+
+for i in range(n - 1):
+    for j in range(n - 1):
+        answer = max(answer, matrix[i][j]+matrix[i][j+1]+matrix[i+1][j])
+
+for i in range(n - 1):
+    for j in range(n - 1):
+        answer = max(answer, matrix[i][j]+matrix[i][j+1]+matrix[i+1][j+1])
+
+for i in range(n):
+    for j in range(n - 2):
+        answer = max(answer, matrix[i][j]+matrix[i][j+1]+matrix[i][j+2])
+
+print(answer)
