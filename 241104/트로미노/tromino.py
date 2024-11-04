e4cd01bd4ce6becee7214e2dce48470a746d85f5
최가온(Gaon Choi) <choigaon1028@gmail.com>
2024-11-04 10:08:03 +0900

    Title: 트로미노, Time: 46ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241104/트로미노/tromino.py
+++ 241104/트로미노/tromino.py
@@ -18,4 +18,8 @@ for i in range(n):
     for j in range(n - 2):
         answer = max(answer, matrix[i][j]+matrix[i][j+1]+matrix[i][j+2])
 
+for i in range(n - 2):
+    for j in range(n):
+        answer = max(answer, matrix[i][j]+matrix[i+1][j]+matrix[i+2][j])
+
 print(answer)
